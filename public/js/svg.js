0832a798840df69e73969cf031103c6dbf4e40ff
yemelitc <yemelitassecyrille@gmail.com>
2025-08-09 16:28:47 +0100

    feat: convert SPA to PWA

--- public/js/svg.js
+++ public/js/svg.js
@@ -1,4 +1,37 @@
 
-var svgStrings = {
-	optionsButton: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false"><path d="M7.41 8.59 12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"></path></svg>',
+let x = window.svgStrings;
+if (!x) {
+	x = {};
+	window.svgStrings = x;
 };
+
+x.optionsButton = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false">
+	<path d="M7.41 8.59 12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" />
+</svg>`;
+
+x.appLogo = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="200" height="200">
+	<!-- two matching half-ellipses (arcs) -->
+	<path
+		d="M15,85 A90,65 0 0 1 185,85"
+		fill="none"
+		stroke="#000"
+		stroke-width="14"
+		stroke-linecap="round"
+	/>
+	<path
+		d="M35,103 A70,55 0 0 1 165,103"
+		fill="none"
+		stroke="#000"
+		stroke-width="14"
+		stroke-linecap="round"
+	/>
+
+	<ellipse cx="100" cy="120" rx="50" ry="30" fill="#000" />
+
+	<circle cx="80"  cy="120" r="5" fill="white" />
+	<circle cx="100" cy="120" r="5" fill="white" />
+	<circle cx="120" cy="120" r="5" fill="white" />
+
+	<polygon points="83,140 117,140 100,175" fill="#000" />
+</svg>`;
+
---
+++ public/sw.js
@@ -0,0 +1,26 @@
+importScripts("/lib/spart/sw-caching.js");
+
+const appAssets = [
+	"/anonymous/chat?g=1",
+	"/favicon.ico",
+	"/icons/logo-192x192.png",
+	"/icons/logo-512x512.png",
+	"/lib/spart/spart.css",
+	"/lib/spart/spart.js",
+	"/lib/spart/fetch.js",
+	"/lib/spart/i18n.js",
+	"/lib/dompurify/purify.min.js",
+	"/lib/marked/marked.min.js",
+	"/lib/prism/prism.js",
+	"/lib/prism/prism.css",
+	"/css/login.css",
+	"/css/chat.css",
+	"/js/login.js",
+	"/js/chat.js",
+	"/js/svg.js",
+	"/i18n/fr.json",
+	"/i18n/en.json",
+	"/manifest.json"
+];
+
+setupCaching(null, appAssets, null);
