657f24fbd095db0b603948d4d7e375da8d3b2ddb
yemelitc <yemelitassecyrille@gmail.com>
2025-09-06 15:58:55 +0100

    enhance: move out startup_init() into libweb

--- startup.c
+++ startup.c
@@ -1,35 +1,33 @@
 #include "startup.h"
-#include <http_core.h>
 
-static void register_controllers()
+/* Called by only one server process at a time to avoid a race condition. */
+static apr_status_t prepare_database(HttpContext *c)
 {
+	database_apply_migrations(&c->dbc, NULL);
+	return errno_to_status_code(errno);
+}
+
+/* Called only by the first HTTP request received by the server process. */
+static apr_status_t prepare_process(HttpContext *c)
+{
+	(void)c; // unused for now
 	register_account_controller();
 	register_message_controller();
 	register_room_controller();
 	register_file_upload_controller();
+	return errno_to_status_code(errno);
 }
 
-static apr_status_t startup_init(HttpContext *c)
-{
-	static bool g_startup_done = false;
-	if (!g_startup_done)
-	{
-		g_startup_done = true;
-
-		register_controllers();
-
-		database_apply_migrations(&c->dbc, NULL);
-	}
-	return errno ? 500 : OK;
-}
-
-/* The handler function for our module. */
+/* The handler function for our module. See module.c */
 apr_status_t http_request_handler(request_rec *r)
 {
-	HttpContext c[1]; // no need for = {0}
-	http_context_init(c, r, NULL, DBMS_MySQL);
+	HttpContext c[1]; // no need to clear
+	http_context_init(c, r, NULL);
 
-	apr_status_t status = startup_init(c);
+	// below must come right after above
+	c->dbc = db_context_init(DBMS_MySQL, NULL);
+
+	apr_status_t status = startup_init(c, prepare_database, prepare_process);
 
 	if (status == OK)
 		status = get_endpoint(c);
